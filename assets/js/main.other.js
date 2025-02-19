7e11159ca7c31dab6a259ae157b7bfecf5bb2d14
irenabc <irenab@live.co.uk>
2025-02-19 23:02:53 +0000

    Add files via upload

---
+++ assets/js/main.other.js
@@ -0,0 +1,41 @@
+$('head')
+    .append('<meta charset="utf-8">')
+    .append('<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">')
+    .append('<meta http-equiv="X-UA-Compatible" content="ie=edge">')
+    .append('<meta name="description" content="Free minimal portfolio web site template,minmal portfolio,porfolio,bootstrap template,html template,photography " />')
+    .append('<title>Home - Free Minimal portfolio template.Art Studio template.</title>')
+    .append('<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800,900&display=swap" rel="stylesheet">')
+    .append('<link rel="stylesheet" href="assets/css/main.css">');
+
+$('footer').append(`
+    <div class="container">
+    <div class="row">
+
+        <div class="col-lg-auto mt-4">
+            <p class="mb-4">sharebootstrap.com</p>
+        </div>
+        <div class="col-lg-3 mt-4 ml-auto">
+            <p>dayy GmbH
+                <br> sarajevska. 32
+                <br> 71000 sarajevo
+                <br> fbih
+            </p>
+                <a href="mailto:hi@yourdomain.com"><span style="text-decoration: underline;">hi@sharebootstrap.com</span></a>
+        </div>
+        <div class="col-lg-3 mt-4 ml-auto">
+            <ul class="list-unstyled footer-link">
+                <li><a href="#">instagram</a></li>
+                <li><a href="#">facebook</a></li>
+                <li><a href="#">twitter</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-auto ml-lg-auto mt-4">
+            <ul class="list-unstyled footer-link">
+                <li><a href="#">instagram</a></li>
+                <li><a href="#">facebook</a></li>
+                <li><a href="#">twitter</a></li>
+            </ul>
+        </div>
+    </div>
+    </div>
+`);
